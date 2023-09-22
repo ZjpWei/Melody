f04ba7acd467f6b22607115e8f3fb169f052b63b
ZjpWei <126215207+ZjpWei@users.noreply.github.com>
2023-09-22 02:22:00 -0500

    Add files via upload

---
+++ figures_script/Figure1_b.R
@@ -0,0 +1,254 @@
+  ##############################################
+#                                                #
+#    Figure 1 (b) Simulation AUPRC Comparison    #
+#                                                #
+  ##############################################
+
+  library('ggplot2')
+  library("tidyverse")
+  library("latex2exp")
+  
+  rm(list = ls())
+  # General
+  load("./CRC_Real/CRC_all_K401/prepare_data/data.rel.all.Rdata")
+  tax.names <- colnames(data.rel[[1]]$Y)
+
+  # signature effect size
+  data.loc <- "./Simulation/By_effsz/"
+  facts <- c("sig2", "sig2.5", "sig3", "sig3.5", "sig4")
+  
+  PRC_all <- data.frame()
+  for(tag in facts){
+    load(paste0(data.loc, tag, "/AUPRC/PRC.Rdata"))
+    tmp.prc <- PRC %>% group_by(method, linetype) %>% summarize(AUPRC = mean(AUPRC))
+    tmp.prc$x.label <- tag
+    PRC_all <- rbind(PRC_all, tmp.prc)
+  }
+  PRC_all$x.label[PRC_all$x.label == "sig2"] <- "1"
+  PRC_all$x.label[PRC_all$x.label == "sig2.5"] <- "1.5"
+  PRC_all$x.label[PRC_all$x.label == "sig3"] <- "2"
+  PRC_all$x.label[PRC_all$x.label == "sig3.5"] <- "2.5"
+  PRC_all$x.label[PRC_all$x.label == "sig4"] <- "3"
+  PRC_all$x.label <- factor(PRC_all$x.label, levels = unique(PRC_all$x.label), ordered = TRUE)
+  PRC_all$method <- factor(PRC_all$method, levels = unique(PRC_all$method), ordered = TRUE)
+  PRC_all <- PRC_all[PRC_all$method %in% c("Melody","ALDEx2","ANCOM-BC","BW", "CLR-LASSO"),]
+  
+  # Customizing the output
+  pdf("./figures/figure1_b_sg_effze.pdf",         # File name
+      width = 4.09, height = 3.57, # Width and height in inches
+      bg = "white")   
+  
+  ggplot(PRC_all, aes(x=x.label, y=AUPRC, group = interaction(method, linetype), linetype = linetype))  +
+    xlab("") + xlab(TeX('$\\Delta$')) + ylim(0.3,0.8) +
+    geom_line(linewidth = 0.7, aes(color=method)) + geom_point(size = 2,aes(color = method), pch = 18) + 
+    scale_color_manual(
+      breaks =  c("Melody", "ALDEx2", "ANCOM-BC","BW","CLR-LASSO"),
+      values = c("red", "green" ,"blue","grey", "orange" ))  +
+    scale_linetype_manual(breaks = c( "Original", "Batch-corrected"), values=c("solid", "dashed")) +
+    theme(panel.grid.major = element_line(colour = "grey", linetype = "dotted"),
+          panel.grid.minor = element_blank(),
+          panel.border = element_rect(colour = "black", fill=NA),
+          axis.line = element_line(colour = "black"),
+          panel.background = element_rect(fill = 'white'),
+          axis.text = element_text(size = 14),
+          axis.title = element_text(size = 14),
+          plot.title = element_text(size = 14),
+          legend.key =  element_rect(colour = "transparent", fill = "transparent"),
+          legend.position = "none",
+          legend.title = element_text(size = 10),
+          legend.text = element_text(size = 10),
+          legend.key.size = unit(0.5, 'cm'), #change legend key size
+          legend.key.height = unit(0.5, 'cm'), #change legend key height
+          legend.key.width = unit(0.75, 'cm')) + 
+    labs(linetype="Pooled data")
+  
+  # Closing the graphical device
+  dev.off() 
+  
+  # signature sparsity
+  data.loc <- "./Simulation/By_taxnum/"
+  facts <- c("d20", "d30", "d40", "d50","d60", "d70","d80")
+  
+  PRC_all <- data.frame()
+  for(tag in facts){
+    load(paste0(data.loc, tag, "/AUPRC/PRC.Rdata"))
+    tmp.prc <- PRC %>% group_by(method, linetype) %>% summarize(AUPRC = mean(AUPRC))
+    tmp.prc$x.label <- tag
+    PRC_all <- rbind(PRC_all, tmp.prc)
+  }
+  PRC_all$x.label[PRC_all$x.label == "d20"] <- "20"
+  PRC_all$x.label[PRC_all$x.label == "d30"] <- "30"
+  PRC_all$x.label[PRC_all$x.label == "d40"] <- "40"
+  PRC_all$x.label[PRC_all$x.label == "d50"] <- "50"
+  PRC_all$x.label[PRC_all$x.label == "d60"] <- "60"
+  PRC_all$x.label[PRC_all$x.label == "d70"] <- "70"
+  PRC_all$x.label[PRC_all$x.label == "d80"] <- "80"
+  PRC_all$x.label <- factor(PRC_all$x.label, levels = unique(PRC_all$x.label), ordered = TRUE)
+  PRC_all$method <- factor(PRC_all$method, levels = unique(PRC_all$method), ordered = TRUE)
+  PRC_all <- PRC_all[PRC_all$method %in% c("Melody","ALDEx2","ANCOM-BC","BW", "CLR-LASSO"),]
+  # Customizing the output
+  pdf("./figures/figure1_b_sg_sparsity.pdf",         # File name
+      width = 4.09, height = 3.57, # Width and height in inches
+      bg = "white")   
+  
+  ggplot(PRC_all, aes(x=x.label, y=AUPRC, group = interaction(method, linetype), linetype = linetype, shape = linetype))  +
+    xlab("") + xlab("d") + ylim(0.3,0.8) +
+    geom_line(linewidth = 0.7, aes(color=method)) + geom_point(size = 2,aes(color = method), pch = 18) +  
+    scale_color_manual(
+      breaks =  c("Melody", "ALDEx2", "ANCOM-BC","BW","CLR-LASSO"),
+      values = c("red", "green" ,"blue","grey", "orange" ))  +
+    scale_linetype_manual(breaks = c( "Original", "Batch-corrected"), values=c("solid", "dashed")) +
+    scale_shape_manual(breaks = c( "Original", "Batch-corrected"), values=c(18,18,20)) +
+    theme(panel.grid.major = element_line(colour = "grey", linetype = "dotted"),
+          panel.grid.minor = element_blank(),
+          panel.border = element_rect(colour = "black", fill=NA),
+          axis.line = element_line(colour = "black"),
+          panel.background = element_rect(fill = 'white'),
+          legend.position ="none",
+          axis.text = element_text(size = 14),
+          axis.title = element_text(size = 14),
+          legend.title = element_text(size = 14),
+          legend.text = element_text(size = 14),
+          plot.title = element_text(size = 14))
+  
+  # Closing the graphical device
+  dev.off() 
+  
+  # signature effect direction
+  data.loc <- "./Simulation/By_pos_prop/"
+  facts <- c("p50","p60", "p70", "p80", "p90","p100")
+  PRC_all <- data.frame()
+  for(tag in facts){
+    load(paste0(data.loc, tag, "/AUPRC/PRC.Rdata"))
+    tmp.prc <- PRC %>% group_by(method, linetype) %>% summarize(AUPRC = mean(AUPRC)) 
+    tmp.prc$x.label <- tag
+    PRC_all <- rbind(PRC_all, tmp.prc)
+  }
+  PRC_all$x.label[PRC_all$x.label == "p50"] <- "50"
+  PRC_all$x.label[PRC_all$x.label == "p60"] <- "60"
+  PRC_all$x.label[PRC_all$x.label == "p70"] <- "70"
+  PRC_all$x.label[PRC_all$x.label == "p80"] <- "80"
+  PRC_all$x.label[PRC_all$x.label == "p90"] <- "90"
+  PRC_all$x.label[PRC_all$x.label == "p100"] <- "100"
+  PRC_all$x.label <- factor(PRC_all$x.label, levels = unique(PRC_all$x.label), ordered = TRUE)
+  PRC_all$method <- factor(PRC_all$method, levels = unique(PRC_all$method), ordered = TRUE)
+  PRC_all <- PRC_all[PRC_all$method %in% c("Melody","ALDEx2","ANCOM-BC","BW", "CLR-LASSO"),]
+  
+  # Customizing the output
+  pdf("./figures/figure1_b_sg_effdir.pdf",         # File name
+      width = 4.09, height = 3.57, # Width and height in inches
+      bg = "white")   
+  
+  ggplot(PRC_all, aes(x=x.label, y=AUPRC, group = interaction(method, linetype), linetype = linetype))  +
+    ylim(0, 1) + xlab("pos%") + ylim(0.3,0.8) +
+    geom_line(linewidth = 0.7, aes(color=method)) + geom_point(size = 2,aes(color = method), pch = 18) + 
+    scale_color_manual(
+      breaks =  c("Melody", "ALDEx2", "ANCOM-BC","BW","CLR-LASSO"),
+      values = c("red", "green" ,"blue","grey", "orange" ))  +
+    scale_linetype_manual(breaks = c( "Original", "Batch-corrected"), values=c("solid", "dashed")) +
+    theme(panel.grid.major = element_line(colour = "grey", linetype = "dotted"),
+          panel.grid.minor = element_blank(),
+          panel.border = element_rect(colour = "black", fill=NA),
+          axis.line = element_line(colour = "black"),
+          panel.background = element_rect(fill = 'white'),
+          legend.position ="none",
+          axis.text = element_text(size = 14),
+          axis.title = element_text(size = 14),
+          legend.title = element_text(size = 14),
+          legend.text = element_text(size = 14),
+          plot.title = element_text(size = 14))
+  
+  # Closing the graphical device
+  dev.off() 
+  
+  # case/control sequence depth unevenness
+  data.loc <- "./Simulation/By_seqdepth/"
+  facts <- c("seq1", "seq1_25", "seq1_50","seq1_75", "seq2")
+  PRC_all <- data.frame()
+  for(tag in facts){
+    load(paste0(data.loc, tag, "/AUPRC/PRC.Rdata"))
+    tmp.prc <- PRC %>% group_by(method, linetype) %>% summarize(AUPRC = mean(AUPRC))
+    tmp.prc$x.label <- tag
+    PRC_all <- rbind(PRC_all, tmp.prc)
+  }
+  PRC_all$x.label[PRC_all$x.label == "seq1"] <- "1"
+  PRC_all$x.label[PRC_all$x.label == "seq1_25"] <- "1.25"
+  PRC_all$x.label[PRC_all$x.label == "seq1_50"] <- "1.5"
+  PRC_all$x.label[PRC_all$x.label == "seq1_75"] <- "1.75"
+  PRC_all$x.label[PRC_all$x.label == "seq2"] <- "2"
+  PRC_all$x.label <- factor(PRC_all$x.label, levels = unique(PRC_all$x.label), ordered = TRUE)
+  PRC_all$method <- factor(PRC_all$method, levels = unique(PRC_all$method), ordered = TRUE)
+  PRC_all <- PRC_all[PRC_all$method %in% c("Melody","ALDEx2","ANCOM-BC","BW", "CLR-LASSO"),]
+  # Customizing the output
+  pdf("./figures/figure1_b_cc_seqdepth.pdf",         # File name
+      width = 4.09, height = 3.57, # Width and height in inches
+      bg = "white")   
+  
+  ggplot(PRC_all, aes(x=x.label, y=AUPRC, group = interaction(method, linetype), linetype = linetype))  +
+    xlab(TeX('$\\mu$')) + ylim(0.3,0.8) +
+    geom_line(linewidth = 0.7, aes(color=method)) + geom_point(size = 2, aes(color = method), pch = 18) + 
+    scale_color_manual(
+      breaks =  c("Melody", "ALDEx2", "ANCOM-BC","BW","CLR-LASSO"),
+      values = c("red", "green" ,"blue","grey", "orange" ))  +
+    scale_linetype_manual(breaks = c( "Original", "Batch-corrected"), values=c("solid", "dashed")) +
+    theme(panel.grid.major = element_line(colour = "grey", linetype = "dotted"),
+          panel.grid.minor = element_blank(),
+          panel.border = element_rect(colour = "black", fill=NA),
+          axis.line = element_line(colour = "black"),
+          panel.background = element_rect(fill = 'white'),
+          legend.position ="none",
+          axis.text = element_text(size = 14),
+          axis.title = element_text(size = 14),
+          legend.title = element_text(size = 14),
+          legend.text = element_text(size = 14),
+          plot.title = element_text(size = 14))
+  
+  # Closing the graphical device
+  dev.off() 
+  
+  # signature prevalence
+  data.loc <- "./Simulation/By_abd_prop/"
+  facts <- c("p20", "p30", "p40", "p50", "p60", "p70","p80")
+  PRC_all <- data.frame()
+  for(tag in facts){
+    load(paste0(data.loc, tag, "/AUPRC/PRC.Rdata"))
+    tmp.prc <- PRC %>% group_by(method, linetype) %>% summarize(AUPRC = mean(AUPRC))
+    tmp.prc$x.label <- tag
+    PRC_all <- rbind(PRC_all, tmp.prc)
+  }
+  PRC_all$x.label[PRC_all$x.label == "p20"] <- "20"
+  PRC_all$x.label[PRC_all$x.label == "p30"] <- "30"
+  PRC_all$x.label[PRC_all$x.label == "p40"] <- "40"
+  PRC_all$x.label[PRC_all$x.label == "p50"] <- "50"
+  PRC_all$x.label[PRC_all$x.label == "p60"] <- "60"
+  PRC_all$x.label[PRC_all$x.label == "p70"] <- "70"
+  PRC_all$x.label[PRC_all$x.label == "p80"] <- "80"
+  PRC_all$x.label <- factor(PRC_all$x.label, levels = unique(PRC_all$x.label), ordered = TRUE)
+  PRC_all$method <- factor(PRC_all$method, levels = unique(PRC_all$method), ordered = TRUE)
+  PRC_all <- PRC_all[PRC_all$method %in% c("Melody","ALDEx2","ANCOM-BC","BW", "CLR-LASSO"),]
+  
+  # Customizing the output
+  pdf("./figures/figure1_b_sg_prev.pdf",         # File name
+      width = 4.09, height = 3.57, # Width and height in inches
+      bg = "white")   
+  
+  ggplot(PRC_all, aes(x=x.label, y=AUPRC, group = interaction(method, linetype), linetype = linetype))  +
+    xlab(TeX('$\\alpha$')) + ylim(0.3,0.8) +
+    geom_line(linewidth = 0.7, aes(color=method)) + geom_point(size = 2, aes(color = method), pch = 18) + scale_color_manual(
+      breaks =  c("Melody", "ALDEx2", "ANCOM-BC","BW","CLR-LASSO"),
+      values = c("red", "green" ,"blue","grey", "orange" ))  +
+    scale_linetype_manual(breaks = c("Original", "Batch-corrected"), values=c("solid", "dashed")) +
+    theme(panel.grid.major = element_line(colour = "grey", linetype = "dotted"),
+          panel.grid.minor = element_blank(),
+          panel.border = element_rect(colour = "black", fill=NA),
+          axis.line = element_line(colour = "black"),
+          panel.background = element_rect(fill = 'white'),
+          legend.position ="none",
+          axis.text = element_text(size = 14),
+          axis.title = element_text(size = 14),
+          legend.title = element_text(size = 14),
+          legend.text = element_text(size = 14),
+          plot.title = element_text(size = 14))
+  
+  # Closing the graphical device
+  dev.off() 
---
+++ figures_script/Figure2_e1_plot.R
@@ -0,0 +1,238 @@
+  ##############################################
+#                                                #
+#    Figure 2 (e1) Stability: All v.s. LOSO      #
+#                                                #
+  ##############################################
+  
+  library("ggplot2")
+  library("tidyverse")
+  library("glmnet")
+  
+  rm(list = ls())
+  # General
+  data.all.loc <- "./CRC_Real/CRC_all_K401/"
+  data.loso.loc <- "./CRC_Real/CRC_loso_K401/"
+  load(paste0(data.all.loc, "prepare_data/data.rel.all.Rdata"))
+  L <- length(data.rel)
+  taxa.names <- colnames(data.rel[[1]]$Y)
+  fdr <- 0.01
+  
+  # Original data
+  ############################### Melody ####################################
+  Jaccard <- c()
+  methods <- c()
+  study <- c()
+  load(paste0(data.all.loc, "Models/Melody.model.all.Rdata"))
+  global.meta <- list(names(which(Melody.model$coef!=0)))
+  meta.rk <- list()
+  for(l in 1:5){
+    load(paste0(data.loso.loc, "Models/Melody.model.", as.character(l),".Rdata"))
+    meta.rk[[l]] <- names(which(Melody.model$coef!=0))
+  }
+  meta.pr <- list()
+  for(l in 1:5){
+    meta.pr[[l]] <- length(intersect(meta.rk[[l]], global.meta[[1]]))/
+      length(unique(c(meta.rk[[l]], global.meta[[1]])))
+  }
+  for(l in 1:L){
+    Jaccard <- c(Jaccard, meta.pr[[l]])
+    methods <- c(methods, "Melody")
+    study <- c(study, as.character(l))
+  }
+  ############################### ANCOM-BC ##################################
+  load(paste0(data.all.loc, "Models/ANCOMBC.model.all.Rdata"))
+  global <- list(which(ANCOMBC.model$res$q_val$labels1 <= fdr))
+  ANCOMBC.rk <- list()
+  for(l in 1:5){
+    load(paste0(data.loso.loc, "Models/ANCOMBC.model.", as.character(l),".Rdata"))
+    ANCOMBC.rk[[l]] <- which(ANCOMBC.model$res$q_val$labels1 <= fdr)
+  }
+  ANCOMBC.pr <- list()
+  for(l in 1:5){
+    ANCOMBC.pr[[l]] <- length(intersect(ANCOMBC.rk[[l]], global[[1]]))/
+      length(unique(c(ANCOMBC.rk[[l]], global[[1]])))
+  }
+  for(l in 1:L){
+    Jaccard <- c(Jaccard, ANCOMBC.pr[[l]])
+    methods <- c(methods, "ANCOM-BC")
+    study <- c(study, as.character(l))
+  }
+  ############################### ALDEx2 ####################################
+  load(paste0(data.all.loc, "Models/Aldex2.model.all.Rdata"))
+  Aldex2.fdr <- Aldex2.model$glmfit$`labels1:pval.BH`
+  global <- list(which(Aldex2.fdr <= fdr))
+  Aldex2.rk <- list()
+  for(l in 1:5){
+    load(paste0(data.loso.loc, "Models/Aldex2.model.", as.character(l), ".Rdata"))
+    Aldex2.fdr <- Aldex2.model$glmfit$`labels1:pval.BH`
+    Aldex2.rk[[l]] <- which(Aldex2.fdr <= fdr)
+  }
+  Aldex2.pr <- list()
+  for(l in 1:5){
+    Aldex2.pr[[l]] <- length(intersect(Aldex2.rk[[l]], global[[1]]))/
+      length(unique(c(Aldex2.rk[[l]], global[[1]])))
+  }
+  for(l in 1:L){
+    Jaccard <- c(Jaccard, Aldex2.pr[[l]])
+    methods <- c(methods, "ALDEx2")
+    study <- c(study, as.character(l))
+  }
+  ############################### BW-prop ###################################
+  load(paste0(data.all.loc, "Models/BW.prop.model.all.Rdata"))
+  global <- list(which(BW.prop.model$q.val <= fdr))
+  BW.rk <- list()
+  for(l in 1:5){
+    load(paste0(data.loso.loc, "Models/BW.prop.model.", as.character(l),".Rdata"))
+    BW.rk[[l]] <- which(BW.prop.model$q.val <= fdr)
+  }
+  BW.pr <- list()
+  for(l in 1:5){
+    BW.pr[[l]] <- length(intersect(BW.rk[[l]], global[[1]]))/length(unique(c(BW.rk[[l]], global[[1]])))
+  }
+  for(l in 1:L){
+    Jaccard <- c(Jaccard, BW.pr[[l]])
+    methods <- c(methods, "BW")
+    study <- c(study, as.character(l))
+  }
+  ############################### CLR-LASSO #################################
+  load(paste0(data.all.loc, "Models/CLR.lasso.model.all.Rdata"))
+  global.clrlasso <- list(names(which(clrlasso$glmnet.fit$beta[,clrlasso$lambda == clrlasso$lambda.min]!=0)))
+  clrlasso.rk <- list()
+  for(l in 1:5){
+    load(paste0(data.loso.loc, "Models/CLR.lasso.model.", as.character(l), ".Rdata"))
+    clrlasso.rk[[l]] <- names(which(clrlasso$glmnet.fit$beta[,clrlasso$lambda == clrlasso$lambda.min]!=0))
+  }
+  clrlasso.pr <- list()
+  for(l in 1:5){
+    clrlasso.pr[[l]] <- length(intersect(clrlasso.rk[[l]], global.clrlasso[[1]]))/
+      length(unique(c(clrlasso.rk[[l]], global.clrlasso[[1]])))
+  }
+  
+  for(l in 1:L){
+    Jaccard <- c(Jaccard, clrlasso.pr[[l]])
+    methods <- c(methods, "CLR-LASSO")
+    study <- c(study, as.character(l))
+  }
+  df1 <- data.frame(Jaccard = Jaccard, methods = methods, study = study, type = "Original")
+
+  # Batch-corrected data
+  ############################### ANCOM-BC ##################################
+  Jaccard <- c()
+  methods <- c()
+  study <- c()
+  load(paste0(data.all.loc, "Models_batch/ANCOMBC.model.all.Rdata"))
+  global <- list(which(ANCOMBC.model$res$q_val$labels1 <= fdr))
+  ANCOMBC.rk <- list()
+  for(l in 1:5){
+    load(paste0(data.loso.loc, "Models_batch/ANCOMBC.model.", as.character(l),".Rdata"))
+    ANCOMBC.rk[[l]] <- which(ANCOMBC.model$res$q_val$labels1 <= fdr)
+  }
+  ANCOMBC.pr <- list()
+  for(l in 1:5){
+    ANCOMBC.pr[[l]] <- length(intersect(ANCOMBC.rk[[l]], global[[1]]))/length(unique(c(ANCOMBC.rk[[l]], global[[1]])))
+  }
+  for(l in 1:L){
+    Jaccard <- c(Jaccard, ANCOMBC.pr[[l]])
+    methods <- c(methods, "ANCOM-BC")
+    study <- c(study, as.character(l))
+  }
+  ############################### ALDEx2 ###################################
+  load(paste0(data.all.loc, "Models_batch/Aldex2.model.all.Rdata"))
+  Aldex2.fdr <- Aldex2.model$glmfit$`labels1:pval.BH`
+  global <- list(which(Aldex2.fdr <= fdr))
+  Aldex2.rk <- list()
+  for(l in 1:5){
+    load(paste0(data.loso.loc, "Models_batch/Aldex2.model.", as.character(l), ".Rdata"))
+    Aldex2.fdr <- Aldex2.model$glmfit$`labels1:pval.BH`
+    Aldex2.rk[[l]] <- which(Aldex2.fdr <= fdr)
+  }
+  Aldex2.pr <- list()
+  for(l in 1:5){
+    Aldex2.pr[[l]] <- length(intersect(Aldex2.rk[[l]], global[[1]]))/
+      length(unique(c(Aldex2.rk[[l]], global[[1]])))
+  }
+  for(l in 1:L){
+    Jaccard <- c(Jaccard, Aldex2.pr[[l]])
+    methods <- c(methods, "ALDEx2")
+    study <- c(study, as.character(l))
+  }
+  ################################ BW-prop ##################################
+  load(paste0(data.all.loc, "Models_batch/BW.prop.model.all.Rdata"))
+  global <- list(which(BW.prop.model$q.val <= fdr))
+  BW.rk <- list()
+  for(l in 1:5){
+    load(paste0(data.loso.loc, "Models_batch/BW.prop.model.", as.character(l),".Rdata"))
+    BW.rk[[l]] <- which(BW.prop.model$q.val <= fdr)
+  }
+  BW.pr <- list()
+  for(l in 1:5){
+    BW.pr[[l]] <- length(intersect(BW.rk[[l]], global[[1]]))/
+      length(unique(c(BW.rk[[l]], global[[1]])))
+  }
+  for(l in 1:L){
+    Jaccard <- c(Jaccard, BW.pr[[l]])
+    methods <- c(methods, "BW")
+    study <- c(study, as.character(l))
+  }
+  ############################### CLR-LASSO #################################
+  load(paste0(data.all.loc, "Models_batch/CLR.lasso.model.all.Rdata"))
+  global.clrlasso <- list(names(which(clrlasso$glmnet.fit$beta[,clrlasso$lambda == clrlasso$lambda.min]!=0)))
+  clrlasso.rk <- list()
+  for(l in 1:5){
+    load(paste0(data.loso.loc, "Models_batch/CLR.lasso.model.", as.character(l), ".Rdata"))
+    clrlasso.rk[[l]] <- names(which(clrlasso$glmnet.fit$beta[,clrlasso$lambda == clrlasso$lambda.min]!=0))
+  }
+  clrlasso.pr <- list()
+  for(l in 1:5){
+    clrlasso.pr[[l]] <- length(intersect(clrlasso.rk[[l]], global.clrlasso[[1]]))/
+      length(unique(c(clrlasso.rk[[l]], global.clrlasso[[1]])))
+  }
+  for(l in 1:L){
+    Jaccard <- c(Jaccard, clrlasso.pr[[l]])
+    methods <- c(methods, "CLR-LASSO")
+    study <- c(study, as.character(l))
+  }
+  df2 <- data.frame(Jaccard = Jaccard, methods = methods, study = study, type = "Batch-corrected")
+  df <- rbind(df1, df2)
+  df$methods <- factor(df$methods, c("Melody", "ALDEx2", "ANCOM-BC","BW","CLR-LASSO"))
+  df$type <- factor(df$type, c("Original", "Batch-corrected"))
+  
+  # Prepare plotting
+  Jac_maxs <- c()
+  Jac_mins <- c()
+  for(l in 1:nrow(df)){
+    Jac_maxs <- c(Jac_maxs, max(df %>% filter(methods == df$methods[l]) %>% 
+                                  filter(type == df$type[l]) %>%
+                                  pull(Jaccard)))
+    Jac_mins <- c(Jac_mins, min(df %>% filter(methods == df$methods[l]) %>% 
+                                  filter(type == df$type[l]) %>%
+                                  pull(Jaccard)))
+  }
+  df$Jac_max <- Jac_maxs
+  df$Jac_min <- Jac_mins
+  
+  # Customizing the output
+  pdf("./figures/figure2_e1.pdf",         # File name
+      width = 4.72, height = 4.45, # Width and height in inches
+      bg = "white")
+  
+  # Creating a plot
+  df  %>% ggplot(aes(x=methods, y=Jaccard, group = type, color = methods)) + 
+    geom_point(size = 1.5, position = position_dodge(width = 0.5)) + ylim(0,1) +
+    geom_errorbar(
+      aes(x = methods,ymin = Jac_min, ymax = Jac_max, color= methods,linetype = type), 
+      width = 0, position = position_dodge(0.5)) + 
+    scale_color_manual(
+      breaks =  c("Melody", "ALDEx2", "ANCOM-BC","BW","CLR-LASSO"),
+      values = c("red", "green" ,"blue","grey", "orange" )
+    ) +  theme(panel.grid.major = element_blank(),
+               panel.grid.minor = element_blank(),
+               axis.title = element_blank(),
+               axis.text.y = element_text(size = 15),
+               axis.text.x = element_text(angle = 45, vjust = 1, hjust=1, size = 12),
+               panel.background = element_rect(fill = 'white'),
+               panel.border = element_rect(colour = "black", fill=NA),
+               legend.position = "none",  text = element_text(size=15))
+  
+  # Closing the graphical device
+  dev.off()
