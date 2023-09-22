8a56ef1c234e36f718b17fda0e5316d295fa1bc0
ZjpWei <126215207+ZjpWei@users.noreply.github.com>
2023-09-22 02:21:36 -0500

    Add files via upload

---
+++ Setup.R
@@ -0,0 +1,44 @@
+  ##############################################
+#                                                #
+#             simulation file setup              #
+#                                                #
+  ##############################################
+
+  # Check directory and create directory
+  # CRC_Real directory
+  loc.vec <- c("./CRC_Real/CRC_all_K401",
+               "./CRC_Real/CRC_all_K849",
+               "./CRC_Real/CRC_all_order",
+               "./CRC_Real/CRC_loso_K401",
+               "./CRC_Real/CRC_loso_K849",
+               "./CRC_Real/CRC_loso_order")
+  
+  for(loc in loc.vec){
+    if(!dir.exists(loc)){
+      dir.create(loc)
+    }
+  }
+  
+  # Prediction directory
+  loc.vec <- c("./Prediction/CRC_locvo_K401",
+               "./Prediction/CRC_ltcvo_K401")
+  
+  for(loc in loc.vec){
+    if(!dir.exists(loc)){
+      dir.create(loc)
+    }
+  }
+  
+  # Simulation directory
+  loc.vec <- c("./Simulation/By_taxnum",
+               "./Simulation/By_pos_prop",
+               "./Simulation/By_abd_prop",
+               "./Simulation/By_effsz",
+               "./Simulation/By_seqdepth")
+  
+  for(loc in loc.vec){
+    if(!dir.exists(loc)){
+      dir.create(loc)
+    }
+  }
+  
