8bdf9fb1de2e4570ede755606879a9c8a17c7e61
ZjpWei <126215207+ZjpWei@users.noreply.github.com>
2023-11-26 13:27:53 -0600

    Add files via upload

---
+++ Simulation/script/Compared_methods_original.R
@@ -0,0 +1,118 @@
+# =============================================== #
+#       Compared methods using original data      #
+# =============================================== #
+
+  # Packages ----
+  library('coin')
+  library('phyloseq')
+  library('pracma')
+  library('corpcor')
+  library('glmnet')
+  library('MASS')
+  library('tidyverse')
+  library('ALDEx2')
+  library("ANCOMBC")
+
+  # General ----
+  cat('Starting model building script\n')
+  start.time <- proc.time()[1]
+  args = commandArgs(trailingOnly=TRUE)
+  if (length(args)==0) {
+    stop("The analysis tag needs to be provided! Exiting...\n")
+  }
+  # arg1: replicate number:
+  # from 1 to 100
+  s <- as.numeric(args[1])
+
+  # arg2: simulation scenario:
+  # signature sparsity: "Sig_number"
+  # signature effect direction: "Sig_effdir"
+  # signature effect size: "Sig_effsz"
+  # case/control sequence depth unevenness: "Sig_depth"
+  scenario <- args[2]
+
+  # arg3: factor for this scenario:
+  loc <- args[3]
+
+  # parameters
+  data.loc <- paste0("./Simulation/", scenario, "/", loc, "/")
+  tag <- "Models_original"
+  if(!dir.exists(paste0(data.loc, tag))){
+    dir.create(paste0(data.loc, tag))
+  }
+
+  # Load data
+  load(paste0(data.loc,"prepare_data/data.rel.", as.character(s), ".Rdata"))
+  L <- length(data.rel)
+  K <- ncol(data.rel[[1]]$Y)
+
+  Study <- NULL
+  Y.pool <- NULL
+  X.pool <- NULL
+  for(l in 1:L){
+    Study <- c(Study, rep(l, length(data.rel[[l]]$X)) )
+    Y.pool <- rbind(Y.pool, data.rel[[l]]$Y)
+    X.pool <- c(X.pool, data.rel[[l]]$X)
+  }
+  
+  ################################# ALDEx2 ###################################
+  # function use for ALDEx2
+  source("./utility/aldex2.R")
+
+  # combine data
+  outcome <- X.pool
+  feature.table <- Y.pool
+  names(outcome) <- rownames(feature.table)
+  feature.table <- data.frame(t(feature.table))
+  meta.data <- data.frame(labels = factor(outcome), study = factor(Study))
+  colnames(feature.table) <- rownames(meta.data)
+
+  ## ALDEx2 will add 0.5 pseudo-count in it's own function.
+  Aldex2.model <- run_aldex2(otu.tab = feature.table, meta = meta.data, formula = "labels + study")
+
+  # save model
+  save(Aldex2.model, file = paste0(data.loc, tag, "/Aldex2.model.",as.character(s),".Rdata"))
+
+  ################################# ANCOM-BC #################################
+  # function use for ANCOM-BC
+  source("./utility/ancombc.R")
+
+  # ANCOM model
+  # ANCOMBC will add 1 as pseudo-count in it's own function;
+  ANCOMBC.model <- ancombc.fun(feature.table = feature.table, meta = meta.data,  formula = "labels + study")
+
+  # save model
+  save(ANCOMBC.model, file = paste0(data.loc, tag, "/ANCOMBC.model.",as.character(s),".Rdata"))
+
+  ################################# CLR-LASSO ################################
+  # Add pseudo-count 0.5
+  Z.pool <- log(Y.pool + 0.5)
+
+  clrx_Z <- apply(Z.pool, 2, function(x) x - rowMeans(Z.pool))
+
+  clrlasso <- cv.glmnet(x = clrx_Z, y = X.pool, nfolds = 5, family = 'binomial')
+
+  save(clrlasso, file = paste0(data.loc, tag, "/CLR.lasso.model.",as.character(s),".Rdata"))
+
+  
+  ################################# BW (prop) ################################
+  source("./utility/rarify.R")
+  Y.rarify <- NULL
+  for(l in 1:L){
+    Y.rarify <- rbind(Y.rarify, .rarefy(otu.tab = data.rel[[l]]$Y, ss = 2023))
+  }
+  
+  # Proportion normalization
+  P.pool <- Y.rarify / rowSums(Y.rarify)
+  pval = NULL
+  for(k in 1:K){
+    BW.data = data.frame(y=P.pool[,k], x = as.factor(X.pool), block=as.factor(Study) )
+    pval = c(pval, pvalue(wilcox_test(y ~ x | block, data=BW.data)) )
+  }
+  # multiple testing correction
+  BW.prop.model <- data.frame(p.val = pval, q.val = p.adjust(pval, method="BH"),
+                              row.names = colnames(P.pool))
+  
+  # save model
+  save(BW.prop.model, file = paste0(data.loc, tag, "/BW.prop.model.",as.character(s),".Rdata"))
+  
---
+++ Simulation/script/Melody_pool.R
@@ -0,0 +1,126 @@
+# =============================================== #
+#           Pooled Meta-Analysis Model            #
+# =============================================== #
+  
+  # Packages ----
+  library("miMeta")
+  library("ConQuR")
+  library("doParallel")
+  library("MMUPHin")
+  
+  # Parameters ----
+  cat('Starting model building script\n')
+  start.time <- proc.time()[1]
+  
+  args = commandArgs(trailingOnly=TRUE)
+  if (length(args)==0) {
+    stop("The analysis tag needs to be provided! Exiting...\n")
+  }
+  # arg1: replicate number:
+  # from 1 to 100
+  s <- as.numeric(args[1])
+  
+  # arg2: simulation scenario:
+  # signature sparsity: "Sig_number"
+  # signature effect direction: "Sig_effdir"
+  # signature effect size: "Sig_effsz"
+  # case/control sequence depth unevenness: "Sig_depth"
+  scenario <- args[2]
+  
+  # arg3: factor for this scenario:
+  loc <- args[3]
+  
+  # General ----
+  data.loc <- paste0("./Simulation/", scenario, "/", loc, "/")
+  tag <- "Models_original"
+  if(!dir.exists(paste0(data.loc, tag))){
+    dir.create(paste0(data.loc, tag))
+  }
+  ref <- "Coprococcus catus [ref_mOTU_v2_4874]"
+  
+  # Original data ----
+  load(paste0(data.loc, "prepare_data/data.rel.", as.character(s), ".Rdata"))
+  data.rel.analysis <- data.rel
+  L <- length(data.rel.analysis)
+  Study <- NULL
+  Group <- NULL
+  rel.abd <- NULL
+  for(l in 1:length(data.rel.analysis)){
+    Study <- c(Study, rep(as.character(l), nrow(data.rel.analysis[[l]]$Y)))
+    Group <- c(Group, as.character(data.rel.analysis[[l]]$X))
+    rel.abd <- rbind(rel.abd, data.rel.analysis[[l]]$Y)
+  }
+  rel.abd <- t(rel.abd)
+  meta <- data.frame(Sample_ID = colnames(rel.abd), Pool = as.character(1),
+                     Study = Study, Group = Group)
+  meta$Study <- factor(meta$Study, levels = c("1", "2", "3", "4", "5"))
+  rownames(meta) <- meta$Sample_ID
+  
+  # Generate summary statistics
+  summary.stat.study <- melody.get.summary(rel.abd = rel.abd,
+                                           sample.data = meta,
+                                           sample.id = "Sample_ID",
+                                           study = "Pool",
+                                           disease = "Group",
+                                           covariates = "Study",
+                                           ref = ref)
+  
+  # Meta-analysis
+  Melody.model <- melody.meta.summary(Melody = summary.stat.study,
+                                      tune.path = "sequence",
+                                      ouput.best.one = FALSE)
+  
+  save(Melody.model, file = paste0(data.loc, tag, "/Melody.pool.org.model.",as.character(s), ".Rdata"))
+
+  # MMUPHin batch-corrected ----
+  tag <- "Models_MMUPHin_batch_corrected"
+  if(!dir.exists(paste0(data.loc, tag))){
+    dir.create(paste0(data.loc, tag))
+  }
+  batch_adj <- MMUPHin::adjust_batch(feature_abd = rel.abd,
+                                     batch = "Study",
+                                     covariates = "Group",
+                                     data = meta)
+  
+  rel.abd.mmu <- batch_adj$feature_abd_adj
+  
+  # Generate summary statistics
+  summary.stat.study.mmu <- melody.get.summary(rel.abd = rel.abd.mmu,
+                                               sample.data = meta,
+                                               sample.id = "Sample_ID",
+                                               study = "Pool",
+                                               disease = "Group",
+                                               covariates = "Study",
+                                               ref = ref)
+  
+  # Meta-analysis
+  Melody.model <- melody.meta.summary(Melody = summary.stat.study,
+                                      tune.path = "sequence",
+                                      ouput.best.one = FALSE)
+  
+  save(Melody.model, file = paste0(data.loc, tag, "/Melody.pool.mmu.model.",as.character(s), ".Rdata"))
+  
+  # ConQuR batch-corrected ----
+  tag <- "Models_ConQuR_batch_corrected"
+  if(!dir.exists(paste0(data.loc, tag))){
+    dir.create(paste0(data.loc, tag))
+  }
+  rel.abd.con <- ConQuR(tax_tab=t(rel.abd), batchid=meta$Study, covariates=meta$Group, 
+                        batch_ref="1", num_core = 2)
+  
+  # Generate summary statistics
+  summary.stat.study.bat <- melody.get.summary(rel.abd = t(rel.abd.con),
+                                               sample.data = meta,
+                                               sample.id = "Sample_ID",
+                                               study = "Pool",
+                                               disease = "Group",
+                                               covariates = "Study",
+                                               ref = ref)
+  # meta-analysis
+  Melody.model <- melody.meta.summary(Melody = summary.stat.study.bat,
+                                      tune.path = "sequence",
+                                      ouput.best.one = FALSE)
+  
+  save(Melody.model, file = paste0(data.loc, tag, "/Melody.pool.con.model.",as.character(s), ".Rdata"))
+  
+  
