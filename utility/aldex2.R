7c4a0ff368069a776d4e7ec4d9af77c97c0c12ab
ZjpWei <126215207+ZjpWei@users.noreply.github.com>
2025-06-18 22:47:22 -0400

    Add files via upload

--- utility/aldex2.R
+++ utility/aldex2.R
@@ -2,15 +2,26 @@ library("tidyverse")
 library("phyloseq")
 library("ALDEx2")
 
-run_aldex2 <- function(otu.tab=feature.table, meta = meta.data, formula){
+run_aldex2 <- function(otu.tab=feature.table, meta = meta.data, formula, gamma = NULL){
   countdata <- otu.tab
   mm <- model.matrix(formula(paste0("~", formula)), meta)
-  aldex.fit <- aldex.clr(countdata, mm, denom="all")
+
+
+  if(is.null(gamma)){
+    message("++ Building aldex.clr object without gamma.")
+  }else{
+    message(paste0("++ Building aldex.clr object with gamma = ", gamma, "."))
+  }
+  aldex.fit <- aldex.clr(countdata, mm, denom="all", gamma = gamma)
+
+  message("++ Calculate glm test statistics.")
   x.e <- aldex.glm(aldex.fit)
+
+  message("++ Calculate effect sizes and difference between all constrast in glm model.")
   glm.effect <- aldex.glm.effect(aldex.fit)
-  
+
   ### adjust p-val by BH, aldex.glm only provide holm q-val.
-  p.fdr <- apply(x.e[,grepl("pval", colnames(x.e)) & !grepl("pval.holm", colnames(x.e))], 
+  p.fdr <- apply(x.e[,grepl("pval", colnames(x.e)) & !grepl("pval.holm", colnames(x.e))],
                  2, p.adjust, method = "BH")
   colnames(p.fdr) <- paste0(colnames(p.fdr), ".BH")
   x.e <- cbind(x.e, p.fdr)
